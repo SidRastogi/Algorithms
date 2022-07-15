28c4bddd620926e76ac2be50dc8249dfb53d630f
sidrastogi <sidrastogi9015@gmail.com>
2022-07-15 23:50:50 +0530

    webpack config with merge short added

--- Scripts/mergeShort2.js
+++ Scripts/mergeShort2.js
@@ -2,7 +2,7 @@ function mergeShort(arr1, arr2) {
   const result = [];
   let i = 0;
   let j = 0;
-  while (i < arr1.lenght && j < arr2.lenght) {
+  while (i < arr1.length && j < arr2.length) {
     if (arr1[i] < arr2[j]) {
       result.push(arr1[i]);
       i++;
@@ -11,8 +11,16 @@ function mergeShort(arr1, arr2) {
       j++;
     }
   }
+  while (i < arr1.length) {
+    result.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    result.push(arr2[j]);
+    j++;
+  }
   return result;
 }
 
-console.log("heeeelo");
+mergeShort([1, 5, 10], [2, 6, 11, 15]);
 console.log(mergeShort([1, 5, 10], [2, 6, 11, 15]));
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  watch: true,
+  mode: 'development',
+  watchOptions: {
+    ignored: /node_modules/,
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 9000,
+  },
+};
